43daa5952b685dd7ba0161365a4f55fde5f984fc
duke-cloud <nobledukex@gmail.com>
2021-08-06 22:12:42 +0100

    first commit

---
+++ stack/static/js/myjs.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  
+    $(".datetimeinput").datepicker({changeYear: true,changeMonth: true, dateFormat: 'yy-mm-dd'});
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+   
+  });
